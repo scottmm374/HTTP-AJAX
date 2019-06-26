9cbad58da907c921d96ada6bd13d7bc0c0f2b14f
Michelle Scott <scottmm374@gmail.com>
2019-06-25 20:15:19 -0400

    added delete and submit button component files

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React, { Component } from 'react';
+import axios from 'axios';
 import FriendsForm from './components/FriendsForm';
-import FriendDetails from './components/FriendDetails';
+import FriendsList from './components/FriendsList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      
-      <div>
-        <FriendDetails />
+export default class App extends Component {
+      state = {
+        savedFriends: []
+    };
+
+    componentDidMount() {
+      axios.get('http://localhost:5000/friends')
+        .then(resp => {
+          this.setState({
+            savedFriends: resp.data
+          })
+        })
+        .catch(err => {
+          console.log('Error', err)
+        })
+    }
+
+  render () {
+    const { friend } = this.state
+    return (
+      <div className="App">
+        <Route path='/' exact render={(props) => <FriendsList {...props} friend={ friend } />} />
+          
+        <div>
+        <FriendsForm />
         </div>
+         
         
-      <div>
-      <FriendsForm />
       </div>
-       
-      
-    </div>
-  );
+    );
+
+  }
+  
 }
 
-export default App;
+
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,35 +1,26 @@
+
 import React from 'react';
-import React, { Component } from 'react';
-import axios from 'axios';
-import FriendDetails from './FriendDetails';
+import { Link } from 'react-router-dom';
 
-export default class FriendsList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            friends: []
-        };
-    }
 
-    componentDidMount () {
-        axios
-        .get('http://localhost:5000/friends')
-        .then(responce => {
-            this.setState(() => ({friends: responce.data }));
-        })
-        .catch(error => {
-            console.error('Server Error', error);
-        });
 
-    }
 
-    render () {}
+export default FriendsList(props)  {
         return (
-            <div>
-                {this.state.friends.map(friend => {
-                    <FriendDetails key={friend.id} friend={friend}/>
-             ))}
+            <div className="friends-card">
+                {props.savedFriends.map((friend => {
+                    <Link to={`/home/${friend.id}`} key={friend.id}>
+                        <p>{ friend.name }</p>
+                        <p>{ friend.age }</p>
+                        <p>{ friend.email }</p>
+                    </Link>
+                ))}}
+                
+                
+             
             </div>
-        );
+        )
+
     }
-}
+
+ 
